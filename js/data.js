688a119ff015e6224b56c95178176cdb20ce483e
aciremaaleman <aciremaaleman@outlook.es>
2018-03-12 12:02:09 -0600

    initializing repository of the Control Light ecommerce

---
+++ js/data.js
@@ -0,0 +1,164 @@
+var data = {
+    products: [
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Blusa CONTROL LIGHT tirantes encaje frente y lateral",
+        id: 1003,
+        price: 199,
+        color: ['blanco', 'negro' , 'nude' , 'azul Marino','fiusha'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  15% Elastano',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Camisa CONTROL LIGHT encaje lateral",
+        id: 1004,
+        price: 179,
+        color: ['blanco', 'negro' , 'nude'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  15% Elastano',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Blusa CONTROL LIGHT manga corta encaje lateral",
+        id: 1005,
+        price: 199,
+        color: ['blanco', 'negro'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  15% Elastano',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Blusa CONTROL LIGHT manga tres cuartos encaje lateral",
+        id: 1006,
+        price: 199,
+        color: ['blanco', 'negro'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  15% Elastano',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Blusa CONTROL LIGHT tirantes encaje lateral y pedreria",
+        id: 1007,
+        price: 199,
+        color: ['negro' , 'nude', 'gris'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  15% Elastano',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Blusa CONTROL LIGHT red lateral y encaje al frente",
+        id: 1008,
+        price: 199,
+        color: ['negro' , 'nude'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  15% Elastano',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Blusa CONTROL RED bicolor ",
+        id: 1009,
+        price: 199,
+        color: ['negro' , 'rojo', 'amarillo'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  15% Elastano',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Pantaleta Faja CONTROL LIGHT corte frances",
+        id: 2001,
+        price: 69,
+        color: ['negro' , 'rojo', 'amarillo'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  12% Elastano y 3% Algodón',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Pantaleta Faja CONTROL LIGHT corte frances algodón",
+        id: 2002,
+        price: 79,
+        color: ['negro' , 'rojo', 'amarillo', 'rosa','nude'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '35% Nylon  12% Elastano y 53% Algodon',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Boxer Faja CONTROL RED bicolor",
+        id: 2004,
+        price: 99,
+        color: ['negro/rojo' , 'negro/amarillo', 'blanco/rojo'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  12% Elastano y 3% Algodon',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Boxer Faja CONTROL LIGHT corte frances",
+        id: 2005,
+        price: 79,
+        color: ['negro' , 'rojo', 'amarillo','blanco','nude'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  12% Elastano y 3% Algodon',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Cinturilla CONTROL LIGHT RED bicolor",
+        id: 2006,
+        price: 99,
+        color: ['negro/plata' , 'negro/oro'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  12% Elastano y 3% Algodon',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Bikini Abundancia",
+        id: 2008,
+        price: 59,
+        color: ['negro' , 'rojo', 'amarillo'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  12% Elastano y 3% Algodon',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Body Boxer CONTROL LIGHT RED de tirantes puente de algodón",
+        id: 6000,
+        price: 240,
+        color: ['negro' , 'nude'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  12% Elastano y 3% Algodon',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Body straples CONTROL LIGHT RED manga corta",
+        id: 6002,
+        price: 240,
+        color: ['negro' , 'nude'],
+        talla : ['CH','MED','GDE','XG', 'XPlus'],
+        composicion: '85% Nylon  12% Elastano y 3% Algodon',
+        stock : 20 
+      },
+      {
+        imageUrl: "http://dummyimage.com/100x100",
+        description: "Baby Doll CONTROL LIGHT con bikini y medias",
+        id: 7000,
+        price: 240,
+        color: ['negro'],
+        talla : ['CH','MED','GDE','XG'],
+        composicion: '85% Nylon  12% Elastano y 3% Algodon',
+        stock : 20 
+      }
+    ]
+  };
