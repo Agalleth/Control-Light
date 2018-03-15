8451eae1af0a84bdf2fc6a68ff2b3a8b6977d6cb
aciremaaleman <aciremaaleman@outlook.es>
2018-03-14 18:46:56 -0600

    Actualizando js

--- js/app.js
+++ js/app.js
@@ -2,30 +2,30 @@ const containerOtherPorductsML = document.getElementById('productsBodiesML');
 let containerPorductsML = document.getElementById('productsML');
 
 fetch(`https://api.mercadolibre.com/sites/MLM/search?q=ropainterior`)
-.then(response => {
-    return response.json();
-})
-.then(data => { 
-    console.log('Request successful');
-    let underWare = data.results;
-    underWare.forEach((element,index) => {
-        // console.log(element)
-        // console.log(index)
-        const id = element.id;
-        const price = element.price;
-        // console.log(id);
-        let productHTML = createProductHTML(element,id,price);
+    .then(response => {
+        return response.json();
     })
-})
-.catch(error => {
-    console.log('Request failed', error)
-});
+    .then(data => {
+        console.log('Request successful');
+        let underWare = data.results;
+        underWare.forEach((element, index) => {
+            // console.log(element)
+            // console.log(index)
+            const id = element.id;
+            const price = element.price;
+            // console.log(id);
+            let productHTML = createProductHTML(element, id, price);
+        })
+    })
+    .catch(error => {
+        console.log('Request failed', error)
+    });
 
-function createProductHTML(element,id,price) {
+function createProductHTML(element, id, price) {
 
     let template =
-  
-    ` <div id="myTabContent" class="tab-content">
+
+        ` <div id="myTabContent" class="tab-content">
     <div role="tabpanel" class="tab-pane fade active in" id="home" aria-labelledby="home-tab">
         <div class="agile_ecommerce_tabs">
             <div class="col-md-4 agile_ecommerce_tab_left">
@@ -53,27 +53,46 @@ function createProductHTML(element,id,price) {
                         <i class="item_price">$ ${price}</i>
                     </p>
                     <div>
-                    <button data-element=("${id}")
+                    <button data-id="${id}" data-price="${price}"
                     onclick='addToCartOne()'
                     class='btn item_add'>Agregar a carrito</button>
                     </div>
                 </div>
             </div> `
-            containerPorductsML.insertAdjacentHTML('beforeEnd',template)
-            // return containerPorductsML;
+    containerPorductsML.insertAdjacentHTML('beforeEnd', template)
+    // return containerPorductsML;
 
 
-    };
+};
 
-// let keptIdAndPrice = localStorage.setItem("products", simpleCart_items);
-// console.log(keptIdAndPrice);
+let array = [];
 
-function addToCartOne(){
-  console.log(event.target)
+function addToCartOne() {
+    let obj = event.target.dataset;
+    // console.log(obj);
+    // let dataId = event.target.dataset.id;
+    // console.log(dataId);
+    // let dataPrice = event.target.dataset.price;
+    // console.log(dataPrice);
 
-// let localStorageProduct = localStorage.setItem('productsTotal', JSON.stringify(id));
-// let infoPrice = localStorage.getItem('productsTotal');
-// console.log(localStorageProduct);
-// console.log(infoPrice);
+
+    array.push(obj);
+    // console.log(array);
+
+    let product = localStorage.setItem("item", JSON.stringify(array))
+    // console.log(product);
+    let getProduct = localStorage.getItem("item");
+    // console.log(getProduct)
+
+    array = JSON.parse(getProduct);
+
+    array.forEach(element => {
+        // console.log(element);
+        let price = element.price;
+        // console.log(price);
+        let id = element.id;
+        // console.log(id);
+        drawProducts(price, id)
+    })
 
 };
---
+++ js/checkout.js
@@ -0,0 +1,18 @@
+const containerProductsBuy = document.getElementById('productsBuy');
+
+
+
+const drawProducts = ((price, id) => {
+    // console.log(price);
+    // console.log(id);
+    // let sum = parseInt(price);
+    // console.log(sum);
+    // summ += sum;
+    // console.log(summ);
+    let summ = 0;
+    // return summ
+    console.log(summ += parseInt(price));
+    
+
+});
+
