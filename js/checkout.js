77cca3d0f365b8930c38d8e52c8b40044422c218
aciremaaleman <aciremaaleman@outlook.es>
2018-03-14 19:21:29 -0600

    Actualizando rama

--- js/checkout.js
+++ js/checkout.js
@@ -1,18 +1,33 @@
 const containerProductsBuy = document.getElementById('productsBuy');
+console.log(containerProductsBuy);
 
 
 
-const drawProducts = ((price, id) => {
+// let template = ``;
+// const drawProducts = ((price, id) => {
+
+// template =
+
+//    ` <div>
+// 		<p>${price}</p>
+//     </div>`
+
+    // containerProductsBuy.innerHTML = template;
+    
+    
+
+
     // console.log(price);
     // console.log(id);
     // let sum = parseInt(price);
     // console.log(sum);
     // summ += sum;
     // console.log(summ);
-    let summ = 0;
-    // return summ
-    console.log(summ += parseInt(price));
+    // let summ = 0;
+    // // return summ
+    // console.log(summ += parseInt(price));
     
 
-});
+// });
+// containerProductsBuy.innerHTML = template;
 
