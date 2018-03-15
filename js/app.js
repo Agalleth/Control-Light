7974fa147b2c304a88ec589d213aa860451fb5ba
lauralm <mein_herz_ist_kalt@hotmail.com>
2018-03-15 02:42:16 -0600

    Subiendo mejoras y arreglando errores

--- js/app.js
+++ js/app.js
@@ -27,8 +27,7 @@ function createProductHTML(element, id, price) {
 
         ` <div id="myTabContent" class="tab-content">
     <div role="tabpanel" class="tab-pane fade active in" id="home" aria-labelledby="home-tab">
-        <div class="agile_ecommerce_tabs">
-            <div class="col-md-4 agile_ecommerce_tab_left">
+            <div class="col-md-4 col-xs-4 col-sm-4 col-lg-4 agile_ecommerce_tab_left">
                 <div class="hs-wrapper">
                     <img src="${element.thumbnail}" alt=" " class="img-responsive" width='100px' />
                     <div class="w3_hs_bottom">
@@ -58,7 +57,9 @@ function createProductHTML(element, id, price) {
                     class='btn item_add'>Agregar a carrito</button>
                     </div>
                 </div>
-            </div> `
+            </div>
+          </div>
+         `
     containerPorductsML.insertAdjacentHTML('beforeEnd', template)
     // return containerPorductsML;
 
@@ -147,92 +148,3 @@ function buttonPaypal() {
     }, '#paypal-button-container');
 
 }
-
-
-// const containerOtherPorductsML = document.getElementById('productsBodiesML');
-// let containerPorductsML = document.getElementById('productsML');
-// fetch(`https://api.mercadolibre.com/sites/MLM/search?q=ropainterior`)
-//     .then(response => {
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log('Request successful');
-//         let underWare = data.results;
-//         underWare.forEach((element, index) => {
-//             // console.log(element)
-//             // console.log(index)
-//             const id = element.id;
-//             const price = element.price;
-//             // console.log(id);
-//             let productHTML = createProductHTML(element, id, price);
-//         })
-//     })
-//     .catch(error => {
-//         console.log('Request failed', error)
-//     });
-// function createProductHTML(element, id, price) {
-//     let template =
-//         ` <div id="myTabContent" class="tab-content">
-//     <div role="tabpanel" class="tab-pane fade active in" id="home" aria-labelledby="home-tab">
-//         <div class="agile_ecommerce_tabs">
-//             <div class="col-md-4 agile_ecommerce_tab_left">
-//                 <div class="hs-wrapper">
-//                     <img src="${element.thumbnail}" alt=" " class="img-responsive" width='100px' />
-//                     <div class="w3_hs_bottom">
-//                         <ul>
-//                             <li>
-//                                 <a href="#" data-toggle="modal" data-target="#myModal">
-//                                     <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-//                                 </a>
-//                             </li>
-//                         </ul>
-//                     </div>
-//                 </div>
-//                 <h5>
-//                     <a href="single.html">Id: ${id}</a>
-//                 </h5>
-//                 <h5>
-//                     <a href="single.html">${element.title}</a>
-//                 </h5>
-//                 <div class="simpleCart_shelfItem">
-//                     <p>
-//                         </span>
-//                         <i class="item_price">$ ${price}</i>
-//                     </p>
-//                     <div>
-//                     <button data-id="${id}" data-price="${price}"
-//                     onclick='addToCartOne()' href='../checkout.html'
-//                     class='btn item_add'>Agregar a carrito</button>
-//                     </div>
-//                 </div>
-//             </div> `
-//     containerPorductsML.insertAdjacentHTML('beforeEnd', template)
-//     // return containerPorductsML;
-// };
-// let array = [];
-// function addToCartOne() {
-//     let obj = event.target.dataset;
-//     // console.log(obj);
-//     // let dataId = event.target.dataset.id;
-//     // console.log(dataId);
-//     // let dataPrice = event.target.dataset.price;
-//     // console.log(dataPrice);
-//     array.push(obj);
-//     // console.log(array);
-//     let product = localStorage.setItem("item", JSON.stringify(array))
-//     // console.log(product);
-//     let getProduct = localStorage.getItem("item");
-//     // console.log(getProduct)
-//     array = JSON.parse(getProduct);
-//     const newArr = array.map(element => {
-//         // console.log(element);
-//         let price = element.price;
-//         // console.log(price);
-//         let id = element.id;
-//         // console.log(id);
-//         return parseInt(price);
-//     })
-//     .reduce((a, b) => a + b, 0);
-//     console.log(newArr);
-//
-// };
--- js/data.js
+++ js/data.js
@@ -1,7 +1,8 @@
-var data = {
-    products: [
+var givens = {
+    newProducts: [
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Blusa",
+        imageUrl: "images/1blusa-tirantes.jpg",
         description: "Blusa CONTROL LIGHT tirantes encaje frente y lateral",
         id: 1003,
         price: 199,
@@ -11,7 +12,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Blusa",
+        imageUrl: "images/2camisa.jpg",
         description: "Camisa CONTROL LIGHT encaje lateral",
         id: 1004,
         price: 179,
@@ -21,7 +23,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Blusa",
+        imageUrl: "images/3blusa-encaje.jpg",
         description: "Blusa CONTROL LIGHT manga corta encaje lateral",
         id: 1005,
         price: 199,
@@ -31,7 +34,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Blusa",
+        imageUrl: "images/4manga-tres-cuartos.jpg",
         description: "Blusa CONTROL LIGHT manga tres cuartos encaje lateral",
         id: 1006,
         price: 199,
@@ -41,7 +45,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Blusa",
+        imageUrl: "images/5blusa.jpg",
         description: "Blusa CONTROL LIGHT tirantes encaje lateral y pedreria",
         id: 1007,
         price: 199,
@@ -51,7 +56,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Blusa",
+        imageUrl: "images/6blusa.jpg",
         description: "Blusa CONTROL LIGHT red lateral y encaje al frente",
         id: 1008,
         price: 199,
@@ -61,7 +67,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Blusa",
+        imageUrl: "images/7blusa.jpg",
         description: "Blusa CONTROL RED bicolor ",
         id: 1009,
         price: 199,
@@ -71,7 +78,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Pantaleta",
+        imageUrl: "images/1pantaleta.jpg",
         description: "Pantaleta Faja CONTROL LIGHT corte frances",
         id: 2001,
         price: 69,
@@ -81,7 +89,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Pantaleta",
+        imageUrl: "images/2pantaleta.jpg",
         description: "Pantaleta Faja CONTROL LIGHT corte frances algodón",
         id: 2002,
         price: 79,
@@ -91,7 +100,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Boxer",
+        imageUrl: "images/1boxer.jpg",
         description: "Boxer Faja CONTROL RED bicolor",
         id: 2004,
         price: 99,
@@ -101,7 +111,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Boxer",
+        imageUrl: "images/2boxer.jpg",
         description: "Boxer Faja CONTROL LIGHT corte frances",
         id: 2005,
         price: 79,
@@ -111,7 +122,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Cinturilla",
+        imageUrl: "images/cinturilla.jpg",
         description: "Cinturilla CONTROL LIGHT RED bicolor",
         id: 2006,
         price: 99,
@@ -121,7 +133,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Bikini",
+        imageUrl: "images/bikini.jpg",
         description: "Bikini Abundancia",
         id: 2008,
         price: 59,
@@ -131,7 +144,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Body",
+        imageUrl: "images/body-boxer.jpg",
         description: "Body Boxer CONTROL LIGHT RED de tirantes puente de algodón",
         id: 6000,
         price: 240,
@@ -141,7 +155,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Body",
+        imageUrl: "images/body-straple.jpg",
         description: "Body straples CONTROL LIGHT RED manga corta",
         id: 6002,
         price: 240,
@@ -151,7 +166,8 @@ var data = {
         stock : 20
       },
       {
-        imageUrl: "http://dummyimage.com/100x100",
+        title: "Baby Doll",
+        imageUrl: "images/babydoll.jpg",
         description: "Baby Doll CONTROL LIGHT con bikini y medias",
         id: 7000,
         price: 240,
@@ -162,3 +178,44 @@ var data = {
       }
     ]
   };
+let boxProducts = document.getElementById('box-products');
+// pintando seccion de nuevos productos
+function newBoxProducts(givens) {
+  let template = ``;
+  let products = givens.newProducts;
+  products.forEach(element => {
+  const idProducts = element.id;
+  const titleProduct = element.title;
+  const descriptionProduct = element.description;
+  const priceProduct = element.price;
+  const pictureProduct = element.imageUrl;
+  const colorProduct = element.color;
+  const sizeProduct = element.talla;
+  const composicionProduct = element.composicion;
+  const stockProduct = element.stock;
+template +=
+` <div id="myTabContent" class="tab-content">
+<div role="tabpanel" class="tab-pane fade active in" id="home" aria-labelledby="home-tab">
+
+        <div class="col-md-4 agile_ecommerce_tab_left">
+            <div class="hs-wrapper">
+                <img src="${pictureProduct}" alt=" " class="img-responsive" width='100px' />
+                <div class="w3_hs_bottom">
+                    <ul>
+                        <li>
+                            <a href="#" data-toggle="modal" data-target="#myModal">
+                                <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <h5>
+                <a href="single.html">${titleProduct}</a>
+            </h5>
+            <h5>${"$" + priceProduct}</h5>
+        </div>`
+boxProducts.innerHTML = template;
+});
+}
+newBoxProducts(givens);
